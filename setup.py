b99881e4f9ec23338ec38fa4967802353d1f21a1
Jesús Gómez <jgomo3@gmail.com>
2012-08-27 22:48:48 -0430

    url para bajar el paquete actualizado.

--- setup.py
+++ setup.py
@@ -8,24 +8,23 @@ Aquellos campos que NO SE VAYAN A UTILIZAR, se deben BORRAR.
 '''
 
 setup(name='fvisusers',
-      version='1.0',
+      version='0.1',
       author='Jesús Gómez',
       author_email='jgomez@funvisis.gob.ve',
       url='http://code.funvisis.gob.ve/funvisis/',
-      download_url='http://code.funvisis.gob.ve/funvisis/LoL',
+      download_url='https://github.com/funvisis/fvislib/tags',
       description='An application for extending djang.auth functionality',
       package_dir={'':'src'},
       packages=['funvisis', 'funvisis.users'],
       requires=['django (>=1.3)'],
       classifiers=['Development Status :: Alpha',
                    'Intended Audience :: Developers',
-                   'Natural Language :: English',
+                   'Natural Language :: Spanish',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python :: 2.6+',
                    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
                    'License :: OSI Approved :: GNU Affero General Public License v3',
-                   'Topic :: Internet',
-                   'Topic :: Scientific/Engineering :: GIS',
+                   'Topic :: Django',
                   ],
 
      )
